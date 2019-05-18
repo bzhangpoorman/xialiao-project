e29a4d94a32e8a72e92e9081fc6bf6ed2823aa03
bzhangpoorman <349016792@qq.com>
2019-05-18 20:39:00 +0800

    新增通用工具类以及二维码生成工具类和二维码配置信息类

---
+++ src/main/java/com/bzhang/xialiao/bean/QrcodeConfig.java
@@ -0,0 +1,7 @@
+package com.bzhang.xialiao.bean;
+
+/**
+ * Created by bzhang on 2019/5/18.
+ */
+public class QrcodeConfig {
+}
---
+++ src/main/java/com/bzhang/xialiao/utils/QrcodeUtils.java
@@ -0,0 +1,7 @@
+package com.bzhang.xialiao.utils;
+
+/**
+ * Created by bzhang on 2019/5/18.
+ */
+public class QrcodeUtils {
+}
